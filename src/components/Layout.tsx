a03c54008b8135ab63276513ff4ddc0c6b8d4425
Mahmoud Ali <mahmoud.ali.spider@gmail.com>
2026-01-05 13:49:30 +0200

    feat: add base layout component with navigation, footer, and route outlet

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,6 @@
 import { Outlet, useLocation } from "react-router-dom";
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
-import { WhatsAppButton } from "./WhatsAppButton";
 import { useEffect } from "react";
 import { cn } from "@/lib/utils";
 
